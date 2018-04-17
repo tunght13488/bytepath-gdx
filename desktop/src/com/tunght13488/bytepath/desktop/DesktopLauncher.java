18fda123a59632f4f26ff1dda4fbe895f10621d9
Ha The Tung <tunght13488@gmail.com>
2018-04-17 16:23:42 +0800

    init project

---
+++ desktop/src/com/tunght13488/bytepath/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.tunght13488.bytepath.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.tunght13488.bytepath.BytepathGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BytepathGame(), config);
+	}
+}
